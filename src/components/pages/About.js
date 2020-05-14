bccb5391609f1e4536c9a866c9e5f9d675451dc0
31pranav31 <pranavdeshpande31@gmail.com>
2020-05-14 13:24:03 +0530

    'second'

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
       <h1>{props.match.params.name}</h1>
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">version 1.0.0</p>
+      <p className="text-secondary">version 2.0.0</p>
     </div>
   );
 };
